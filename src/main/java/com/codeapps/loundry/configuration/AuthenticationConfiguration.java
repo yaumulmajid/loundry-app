a9986be4e4345b046295fb7c33ecf447effffaca
y-majid16 <yaumulmajid@gmail.com>
2024-01-19 10:44:29 +0700

    add authority Controller

--- src/main/java/com/codeapps/loundry/configuration/AuthenticationConfiguration.java
+++ src/main/java/com/codeapps/loundry/configuration/AuthenticationConfiguration.java
@@ -1,6 +1,5 @@
 package com.codeapps.loundry.configuration;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
